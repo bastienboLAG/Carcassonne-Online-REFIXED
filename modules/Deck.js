b943c35bf851b34646dfd31f0bd2bf73b1bbfc38
bastienbo <109655148+bastienboLAG@users.noreply.github.com>
2026-03-12 21:43:04 +0100

    Update Deck.js

--- modules/Deck.js
+++ modules/Deck.js
@@ -171,7 +171,7 @@ export class Deck {
                     normalDeck.push({ id: 'inns_cathedrals-03', zones: data.zones, imagePath: data.image });
                 } catch(e) { console.error('Erreur chargement inns_cathedrals-03:', e); }
             }
-            const testIds = ['base-24', 'dragon-10'];
+            const testIds = ['base-02', 'base-02', 'dragon-01, 'base-04', 'base-04', 'base-04', 'base-04', 'base-04'];
             this.tiles = testIds.map(id => {
                 const found = normalDeck.find(t => t.id === id);
                 return found ? { ...found } : null;
